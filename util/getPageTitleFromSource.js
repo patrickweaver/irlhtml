86b52c10d4415c5866919edc077da583b5312995
Patrick Weaver <pjpwea@gmail.com>
2024-06-22 14:24:53 -0700

    Set up jest, test title parsing

--- util/getPageTitleFromSource.js
+++ util/getPageTitleFromSource.js
@@ -1,11 +1,14 @@
-function getPageTitleFromSource(_source) {
-	if (!_source) return;
-	const source = _source.toLowerCase();
-	const splitOnTitleOpen = source.split("<title>");
-	if (splitOnTitleOpen?.length !== 2) return;
-	const splitOnTitleClose = splitOnTitleOpen[1].split("</title>");
-	if (splitOnTitleClose?.length !== 2) return;
-	return splitOnTitleClose[0];
+function getPageTitleFromSource(source) {
+	const openTag = "<title>";
+	const closeTag = "</title>";
+	if (!source) return;
+	const sourceLowercase = source.toLowerCase();
+	const titleOpenIndex = sourceLowercase.indexOf(openTag);
+	if (titleOpenIndex === -1) return;
+	const titleCloseIndex = sourceLowercase.indexOf(closeTag);
+	if (titleCloseIndex === -1) return;
+	const title = source.slice(titleOpenIndex + openTag.length, titleCloseIndex);
+	return title.trim().replace("  ", " ").replace("	", " ");
 }
 
 module.exports = getPageTitleFromSource;
