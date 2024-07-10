670f38e929fe87c9f0ea3ce5830954fb5c3fa3dc
Patrick Weaver <pjpwea@gmail.com>
2024-07-10 07:31:05 -0700

    Update prompt

--- src/ocr/prompt.ts
+++ src/ocr/prompt.ts
@@ -1,2 +1,2 @@
 export const PROMPT =
-	"This is an image of a piece of paper with HTML code on it.	If there are any syntax errors, fix them with the most likely valid HTML. Respond with just the HTML code property formatted, not wrapped in markdown.";
+	"This is an image of a piece of paper with HTML code on it.	If there are any syntax errors, fix them with the most likely valid HTML. Respond with just the HTML code property formatted, not wrapped in markdown or any description of what is in the response.";
