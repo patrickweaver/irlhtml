f116bbdfc65f599c0c33c09779a0d73d51b649e3
Patrick Weaver <pjpwea@gmail.com>
2025-06-13 17:21:05 -0700

    Update style

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import mainRoutes from "./routes/main";
 
 const app: Express = express();
 app.use(express.json({ limit: "15mb" }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 app.set("view engine", "ejs");
 app.set("views", "views");
 
